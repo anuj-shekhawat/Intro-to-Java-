0e3e1efb6bccd4c67b02c0c02a3d40725e81c4f6
Anuj Shekhawat <54101776+anuj-shekhawat@users.noreply.github.com>
2020-05-17 14:14:22 +0530

    Create Question_1.java

---
+++ CH-1/Question_1.java
@@ -0,0 +1,5 @@
+public class Question_1{
+    public static void main(final String[] args) {
+        System.out.println("Welcome to Java,Welcome to Computer Science, and Programming is fun.");
+    }
+}
