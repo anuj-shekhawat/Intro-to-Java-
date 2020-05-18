91d37d8e992b951f4ee270881d24f005ac4fd8dd
Anuj Shekhawat <54101776+anuj-shekhawat@users.noreply.github.com>
2020-05-18 12:14:09 +0530

    answer of ch 2

---
+++ CH-2/Question_8.java
@@ -0,0 +1,36 @@
+package CH-2;
+
+public class Question_8 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);	// Create new Scanner object
+
+
+		System.out.print("Enter the time zone offset to GMT: ");
+		int offset = input.nextInt();
+
+
+		long totalMilliseconds = System.currentTimeMillis();
+
+		
+		long totalSeconds = totalMilliseconds / 1000;
+
+
+		long currentSecond = totalSeconds % 60;
+
+		long totalMinutes = totalSeconds / 60;
+
+
+		long currentMinute = totalMinutes % 60;
+
+
+		long totalHours = totalMinutes / 60;
+
+
+		long currentHour = totalHours % 24;
+		currentHour = currentHour + offset;
+
+		System.out.println("Current time is " + currentHour + ":"
+			+ currentMinute + ":" + currentSecond);
+    }
+}
